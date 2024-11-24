6545f6f2cdb700fe5a6852aa35a5fe68bd2b70f9
Surendiran2710 <surendiran2710@gmail.com>
2024-11-24 22:14:34 +0530

    first

---
+++ src/components/TodoContainer.jsx
@@ -0,0 +1,40 @@
+import AddTodoForm from "./AddTodoForm"
+import TodoList from "./TodoList"
+import { useState } from "react"
+
+
+function TodoContainer() {
+
+    const [activityArr, setActivityArr] = useState([
+        {
+            id: 1,
+            activity: "Go for a walk"
+        },
+        {
+            id: 2,
+            activity: "Have Bath"
+        },
+        {
+            id: 3,
+            activity: "Have Breakfast"
+        }
+    ])
+
+
+    return (
+
+        <div>
+
+            <div className="flex gap-5 flex-wrap">
+                <AddTodoForm activityArr={activityArr} setActivityArr={setActivityArr} />
+
+                <TodoList activityArr={activityArr} setActivityArr={setActivityArr} />
+
+            </div>
+
+        </div>
+
+    )
+}
+
+export default TodoContainer
---
+++ src/pages/Landing.jsx
@@ -0,0 +1,33 @@
+import Header from "../components/Header"
+import Card from "../components/Card"
+import TodoContainer from "../components/TodoContainer"
+import { useLocation } from "react-router-dom"
+
+function Landing()
+{
+    const data = useLocation()
+   
+
+    return(
+        <div className="bg-black p-16">
+      <div className="bg-[#EFEFEF] p-10 border rounded-md">
+        {/* Header */}
+
+        <Header username={data.state.user}/>
+        {/*Card*/}
+
+        <div className="flex justify-between gap-7 my-5 flex-wrap">
+          <Card bgcolor={"#8272DA"} title={"23°"} subtitle={"Chennai"} />
+          <Card bgcolor={"#FD6663"} title={"November"} subtitle={"09:00 PM"} />
+          <Card bgcolor={"#FCA201"} title={"Built Using"} subtitle={"React"} />
+        </div>
+
+      
+        <TodoContainer/>
+      </div>
+
+    </div>
+    )
+}
+
+export default Landing 
